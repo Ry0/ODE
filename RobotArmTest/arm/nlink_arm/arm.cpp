18a42a6b1f3af5638cd1ae45f17ead82885a4065
Ry0 <k104037r@mail.kyutech.jp>
2015-04-07 22:23:22 +0900

    nリンクアームの姿勢をランダムに生成するようにしたよ

--- RobotArmTest/arm/nlink_arm/arm.cpp
+++ RobotArmTest/arm/nlink_arm/arm.cpp
@@ -7,52 +7,40 @@
 #ifdef dDOUBLE
 #define dsDrawCapsule dsDrawCapsuleD
 #endif
-#define NUM 40                          // リンク数
+#define NUM 50                          // リンク数
 
-dWorldID      world;                   // 動力学計算用のワールド
-dSpaceID      space;                   // 衝突検出用のスペース
-dGeomID       ground;                  // 地面のジオメトリID番号
-dJointGroupID contactgroup;            // 接触点グループ
-dJointID      joint[NUM];              // ジョイントのID番号
-dsFunctions   fn;                      // ドロースタッフの描画関数
+dWorldID      world;                    // 動力学計算用のワールド
+dSpaceID      space;                    // 衝突検出用のスペース
+dGeomID       ground;                   // 地面のジオメトリID番号
+dJointGroupID contactgroup;             // 接触点グループ
+dJointID      joint[NUM];               // ジョイントのID番号
+dsFunctions   fn;                       // ドロースタッフの描画関数
 
 typedef struct {
-  dBodyID body;                        // ボディのID番号
-  dGeomID geom;                        // ジオメトリのID番号
-} MyObject;                            // MyObject構造体
+  dBodyID body;                         // ボディのID番号
+  dGeomID geom;                         // ジオメトリのID番号
+} MyObject;                             // MyObject構造体
 
-MyObject rlink[NUM];                   // リンク
-dReal  THETA[NUM] = {0.0};             // 関節の目標角度[rad]
+MyObject rlink[NUM];                    // リンク
+dReal  THETA[NUM][2];                   // 関節の目標角度[rad]
+bool flag = false;
 
 /*** ロボットアームの生成 ***/
 void  makeArm()
 {
   dMass mass;                                    // 質量パラメータ
-  // dReal x[NUM]      = {0.00, 0.00, 0.00, 0.00};  // 重心 x
-  // dReal y[NUM]      = {0.00, 0.00, 0.00, 0.00};  // 重心 y
-  // dReal z[NUM]      = {0.05, 0.15, 0.25, 0.35};  // 重心 z
-  // dReal length[NUM] = {0.10, 0.10, 0.10, 0.10};  // 長さ
-  // dReal weight[NUM] = {2.00, 2.00, 2.00, 2.00};  // 質量
-  // dReal r[NUM]      = {0.04, 0.04, 0.04, 0.04};  // 半径
-  // dReal c_x[NUM]    = {0.00, 0.00, 0.00, 0.00};  // 関節中心点 x
-  // dReal c_y[NUM]    = {0.00, 0.00, 0.00, 0.00};  // 関節中心点 y
-  // dReal c_z[NUM]    = {0.00, 0.10, 0.20, 0.40};  // 関節中心点 z
-  // dReal axis_x[NUM] = {0, 0, 0, 0};              // 関節回転軸 x
-  // dReal axis_y[NUM] = {0, 0, 1, 1};              // 関節回転軸 y
-  // dReal axis_z[NUM] = {1, 1, 0, 0};              // 関節回転軸 z
-  dReal x[NUM];
-  dReal y[NUM];
-  dReal z[NUM];
-  dReal length[NUM];
-  dReal weight[NUM];
-  dReal r[NUM];
-  dReal c_x[NUM];
-  dReal c_y[NUM];
-  dReal c_z[NUM];
-  dReal axis_x[NUM];
-  dReal axis_y[NUM];
-  dReal axis_z[NUM];
-
+  dReal x[NUM];                                  // 重心 x
+  dReal y[NUM];                                  // 重心 y
+  dReal z[NUM];                                  // 重心 z
+  dReal length[NUM];                             // 長さ
+  dReal weight[NUM];                             // 質量
+  dReal r[NUM];                                  // 半径
+  dReal c_x[NUM];                                // 関節中心点 x
+  dReal c_y[NUM];                                // 関節中心点 y
+  dReal c_z[NUM];                                // 関節中心点 z
+  dReal axis_x[NUM];                             // 関節回転軸 x
+  dReal axis_y[NUM];                             // 関節回転軸 y
+  dReal axis_z[NUM];                             // 関節回転軸 z
 
   for (int i = 0; i < NUM; i++) {
     x[i] = 0.0;
@@ -65,8 +53,8 @@ void  makeArm()
     c_y[i] = 0.0;
     c_z[i] = 0.10*i;
     axis_x[i] = 0.0;
-    axis_y[i] = 1;
-    axis_z[i] = 0;
+    axis_y[i] = 1.0;
+    axis_z[i] = 0.0;
   }
 
 
@@ -86,10 +74,11 @@ void  makeArm()
   dJointAttach(joint[0], rlink[0].body, 0);
   dJointSetFixed(joint[0]);
   for (int j = 1; j < NUM; j++) {
-    joint[j] = dJointCreateHinge(world, 0); // ヒンジジョイント
+    joint[j] = dJointCreateUniversal(world, 0); // ヒンジジョイント
     dJointAttach(joint[j], rlink[j].body, rlink[j-1].body);
-    dJointSetHingeAnchor(joint[j], c_x[j], c_y[j], c_z[j]);
-    dJointSetHingeAxis(joint[j], axis_x[j], axis_y[j],axis_z[j]);
+    dJointSetUniversalAnchor(joint[j], c_x[j], c_y[j], c_z[j]);
+    dJointSetUniversalAxis1(joint[j], axis_x[j], axis_y[j], axis_z[j]);
+    dJointSetUniversalAxis2(joint[j], axis_y[j], axis_x[j], axis_z[j]);
   }
 }
 
@@ -103,7 +92,7 @@ void drawArm()
      dsDrawCapsule(dBodyGetPosition(rlink[i].body),
      dBodyGetRotation(rlink[i].body),length,r);
      if(i%2 == 0){
-      dsSetColor (1, 0, 0);
+      dsSetColor (0.8, 0.8, 0.8);
      }else{
       dsSetColor (0.9, 0.9, 0.9);
      }
@@ -114,12 +103,18 @@ void drawArm()
 void Pcontrol()
 {
   dReal k =  10.0, fMax = 100.0;                   // 比例ゲイン，最大トルク
-
+  dReal tmp[2];
+  dReal z[2];
   for (int j = 1; j < NUM; j++) {
-    dReal tmp = dJointGetHingeAngle(joint[j]);     // 関節角の取得
-    dReal z = THETA[j] - tmp;                      // 残差
-    dJointSetHingeParam(joint[j],dParamVel, k*z);  // 角速度の設定
-    dJointSetHingeParam(joint[j],dParamFMax,fMax); // トルクの設定
+    tmp[0] = dJointGetUniversalAngle1(joint[j]);     // 関節角の取得
+    z[0] = THETA[j][0] - tmp[0];                      // 残差
+    dJointSetUniversalParam(joint[j],dParamVel, k*z[0]);  // 角速度の設定
+    dJointSetUniversalParam(joint[j], dParamFMax, fMax);  // トルクの設定
+
+    tmp[1] = dJointGetUniversalAngle2(joint[j]);              // 関節角の取得
+    z[1] = THETA[j][1] - tmp[1];                              // 残差
+    dJointSetUniversalParam(joint[j], dParamVel2, k * z[1]);  // 角速度の設定
+    dJointSetUniversalParam(joint[j], dParamFMax2, fMax);     // トルクの設定
   }
 }
 
@@ -134,9 +129,13 @@ double GetRandom(double min,double max, int digit)
 
 void rand_theta(){
   srand((unsigned int)time(NULL));
-  for (int i = 0; i < NUM; ++i){
-    THETA[i] = M_PI*0.2*GetRandom(-1, 1, 5);
-    // printf("THETA[%d] = %lf\n", i, THETA[i] );
+  if(flag == false){
+    for (int i = 0; i < NUM; ++i){
+      THETA[i][0] = M_PI*0.2*GetRandom(-1, 1, 5);
+      THETA[i][1] = M_PI*0.2*GetRandom(-1, 1, 5);
+      // printf("THETA[%d] = %lf\n", i, THETA[i] );
+    }
+    flag = true;
   }
 }
 
@@ -152,18 +151,20 @@ void start()
 void command(int cmd)
 {
   switch (cmd) {
-    case 'j': THETA[1] += M_PI/180; break;     // jキー
-    case 'f': THETA[1] -= M_PI/180; break;     // fキー
-    case 'k': THETA[2] += M_PI/180; break;     // kキー
-    case 'd': THETA[2] -= M_PI/180; break;     // dキー
-    case 'l': THETA[3] += M_PI/180; break;     // lキー
-    case 's': THETA[3] -= M_PI/180; break;     // sキー
+    case 'j': THETA[1][0] += M_PI/180; break;     // jキー
+    case 'f': THETA[1][0] -= M_PI/180; break;     // fキー
+    case 'k': THETA[2][0] += M_PI/180; break;     // kキー
+    case 'd': THETA[2][0] -= M_PI/180; break;     // dキー
+    case 'l': THETA[3][0] += M_PI/180; break;     // lキー
+    case 's': THETA[3][0] -= M_PI/180; break;     // sキー
+    case 'r': flag = false; break;     // sキー
   }
 }
 
 /*** シミュレーションループ ***/
 void simLoop(int pause)
 {
+  rand_theta();
   Pcontrol();                                  // P制御
   dWorldStep(world, 0.01);                     // 動力学計算
   drawArm();                                   // ロボットの描画
@@ -188,7 +189,12 @@ int main(int argc, char **argv)
   contactgroup = dJointGroupCreate(0);            // 接触グループの生成
   ground       = dCreatePlane(space, 0, 0, 1, 0); // 地面の生成
   dWorldSetGravity(world, 0, 0, -9.8);            // 重力の設定
-  rand_theta();
+
+  // for (int i = 0; i < NUM; ++i){
+  //   for (int j = 0; j < 2; ++j){
+  //     THETA[i][j] = M_PI*0.01*(j+1);
+  //   }
+  // }
   makeArm();                                      // アームの生成
   dsSimulationLoop(argc, argv, 640, 480, &fn);    // シミュレーションループ
   dSpaceDestroy(space);                           // スペースの破壊
